643406860064a61e1b0b673586ac19e14e46a904
Alexander Mozhaev <aumozhaev@gmail.com>
2018-01-15 14:31:31 +0300

    mysql

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=test_hsh',
+            'username' => 'test_hsh_user',
+            'password' => 'test_hsh_user_Pass1!',
             'charset' => 'utf8',
         ],
         'mailer' => [
